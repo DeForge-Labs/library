7a43ec11e72620e9df50b14b1af5f3ce0fe61cc3
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-04-12 02:09:07 +0530

    Adding base node class

---
+++ BaseNode/index.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(name, inputType, outputType) {
+        
+    }
+
+    /**
+     * The main method that runs all nodes.
+     * Need to be overidden.
+     */
+    run(params) {
+        
+    }
+}
