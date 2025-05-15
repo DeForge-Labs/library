dd9ca6db33a3c32cc447c9384b109e7777bd3d1d
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-05-15 11:54:07 +0530

    feat: add rag node

---
+++ processing/rag_node/node.js
@@ -0,0 +1,144 @@
+import BaseNode from "../../core/BaseNode/node.js";
+import { embedMany } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { LibSQLVector } from '@mastra/core/vector/libsql';
+import { MDocument } from '@mastra/rag'
+import { Downloader } from "nodejs-file-downloader";
+import { exec } from 'child_process';
+import fs from 'fs';
+import crypto from 'crypto';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+    title: "Knowledge Base",
+    category: "processing",
+    type: "rag_node",
+    icon: {},
+    desc: "chat with LLMs",
+    inputs: [],
+    outputs: [
+        {
+            desc: "Vector database",
+            name: "output",
+            type: "RAG",
+        },
+        {
+            desc: "database fileName",
+            name:"rag_db_name",
+            type: "Text",
+        },
+    ],
+    fields: [
+        {
+            desc: "The knowledge base file",
+            name: "file",
+            type: "File",
+            value: "",
+        },
+    ],
+    difficulty: "medium",
+    tags: ['api', 'llm', 'chatbot'],
+}
+
+class rag_node extends BaseNode {
+    constructor() {
+        super(config);
+    }
+
+    async run(inputs, contents, webconsole, serverData) {
+        let isEmbedded = false;
+        let fileHash = "";
+
+        const workflowId = serverData.workflowId.replaceAll("-", "_");
+
+        const fileUrl = contents[0].value;
+        const allowedTypes = ["pdf", "docx", "txt", "json", "md"];
+
+        const downloader = new Downloader({
+            url: fileUrl,
+            directory: "./",
+            cloneFiles: false,
+            onBeforeSave: (fileName) => {
+                fileHash = crypto.createHash('sha256').update(fileName).digest('hex');
+
+                if (fs.existsSync(`./${workflowId}_${fileHash}.db`)) {
+                    isEmbedded = true;
+                }
+            }
+        });
+
+        const store = new LibSQLVector({
+            connectionUrl: `file:./${workflowId}_${fileHash}.db`,
+        });
+
+        if (isEmbedded) {
+            webconsole.success("RAG NODE | DB exists");
+            return {
+                store: store,
+                fileName: `./${workflowId}_${fileHash}.db`
+            };
+        }
+
+        try {
+            const { filePath, downloadStatus } = await downloader.download();
+
+            if (!allowedTypes.some(type => filePath.endsWith(type))) {
+                webconsole.error("RAG NODE | Unsupported file type.");
+                return null;
+            }
+
+            const command = `markitdown ${filePath} -o document.md`;
+            exec(command, (error, stdout, stderr) => {
+                    if (error) {
+                        webconsole.error(`Error executing command: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        webconsole.error(`Command stderr: ${stderr}`);
+                        return;
+                    }
+                    webconsole.info(`Command stdout: ${stdout}`);
+                });
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    webconsole.error("RAG NODE | error deleting the uploaded file");
+                }
+            });
+
+            const data = fs.readFileSync('./document.md', 'utf-8');
+            const doc = MDocument.fromMarkdown(data);
+            const chunks = await doc.chunk();
+
+            const { embeddings } = await embedMany({
+                model: openai.embedding("text-embedding-3-small"),
+                values: chunks.map((chunk) => chunk.text),
+            });
+
+            await store.createIndex({
+                indexName: `collection`,
+                dimension: 1536,
+            });
+
+            await store.upsert({
+                indexName: `collection`,
+                vectors: embeddings,
+                metadata: chunks.map(chunk => ({ text: chunk.text }))
+            });
+
+            webconsole.success("RAG NODE | Vector DB created");
+
+            return {
+                store: store,
+                fileName: `./${workflowId}_${fileHash}.db`,
+            };
+
+        } catch (error) {
+            webconsole.error("RAG NODE | some error occured: ", error);
+            return null;
+        }
+    }
+}
+
+export default rag_node;
