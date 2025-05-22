cb3d8bd1e2b1739dfc71794cb93fbb374f5b6608
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-05-22 17:12:29 +0530

    feat: add tg trigger template

---
+++ trigger/tg_trigger/node.js
@@ -0,0 +1,44 @@
+import BaseNode from "../../core/BaseNode/node.js";
+
+const config = {
+    title: "Telegram Trigger",
+    category: "trigger",
+    type: "tg_trigger",
+    icon: {},
+    desc: "Triggers the flow when a message is recieved on the telegram bot",
+    inputs: [],
+    outputs: [
+        {
+            desc: "The Flow to trigger",
+            name: "Flow",
+            type: "Flow",
+        },
+        {
+            desc: "Message recieved by the bot",
+            name: "Message",
+            type: "Text",
+        },
+    ],
+    fields: [
+        {
+            desc: "Text to send when recieving the /start command",
+            name: "On Start",
+            type: "TextArea",
+            value: "text here ...",
+        },
+    ],
+    difficulty: "easy",
+    tags: ["trigger", "telegram"],
+}
+
+class tg_trigger extends BaseNode {
+    constructor() {
+        super(config);
+    }
+
+    async run(inputs, contents, webconsole, serverData) {
+        
+    }
+}
+
+export default tg_trigger;
