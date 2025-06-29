9daf13195d7a4c8faaf3b6e3dedd57ed5367862c
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-06-29 23:23:01 +0530

    feat: add function to cron trigger

--- trigger/cron_trigger/node.js
+++ trigger/cron_trigger/node.js
@@ -38,6 +38,25 @@ class cron_trigger extends BaseNode {
 
     async run(inputs, contents, webconsole, serverData) {
         
+        webconsole.success("CRON TRIGGER NODE | Executing job");
+
+        const invokeTimestamp = serverData.invokeTime || Date.now();
+        const dateObj = new Date(invokeTimestamp);
+
+        const date = {
+            year: dateObj.getFullYear(),
+            month: dateObj.getMonth() + 1,
+            day: dateObj.getDate(),
+            hour: dateObj.getHours(),
+            minute: dateObj.getMinutes(),
+            second: dateObj.getSeconds(),
+            millisecond: dateObj.getMilliseconds(),
+        };
+
+        return {
+            "Flow": true,
+            "timeStamp": date,
+        };
     }
 }
 
