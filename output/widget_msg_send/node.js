1503f9e8137d41629ae1b76bdfcb3c1bf3045b20
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-07-18 00:49:00 +0530

    feat: widget nodes added

---
+++ output/widget_msg_send/node.js
@@ -0,0 +1,83 @@
+import BaseNode from "../../core/BaseNode/node.js";
+
+const config = {
+    title: "Send Widget Response",
+    category: "output",
+    type: "widget_msg_send",
+    icon: {},
+    desc: "Send a response to your support bot widget",
+    inputs: [
+        {
+            desc: "The flow of the workflow",
+            name: "Flow",
+            type: "Flow",
+        },
+        {
+            desc: "Text to send",
+            name: "Message",
+            type: "Text",
+        },
+    ],
+    outputs: [
+        {
+            desc: "The final output object of chat bot",
+            name: "Result",
+            type: "JSON"
+        },
+    ],
+    fields: [
+        {
+            desc: "Text to send",
+            name: "Message",
+            type: "TextArea",
+            value: "text here ...",
+        },
+    ],
+    difficulty: "easy",
+    tags: ["output", "support", "bot", "widget"],
+}
+
+class widget_msg_send extends BaseNode {
+    constructor() {
+        super(config);
+    }
+
+    async run(inputs, contents, webconsole, serverData) {
+        webconsole.info("WIDGET MSG NODE | Started execution");
+
+        const MessageFilter = inputs.filter((e) => e.name === "Message");
+        const Message = MessageFilter.length > 0 ? MessageFilter[0].value : contents.filter((e) => e.name === "Message")[0].value || "";
+
+        if (!Message) {
+            webconsole.error("WIDGET MSG NODE | Message contents empty");
+            return null;
+        }
+
+        const ChatId = serverData.chatbotPayload?.queryId || "";
+        
+        if (!ChatId) {
+            webconsole.error("WIDGET MSG NODE | No Chat ID found");
+            return null;
+        }
+
+        try {
+            
+            const responseObj = {
+                "Message": Message,
+                "ChatID": ChatId,
+            };
+
+            webconsole.success("WIDGET MSG NODE | Successfully generated and emitted respons object");
+
+            return {
+                "Result": responseObj
+            };
+        
+        } catch (error) {
+            webconsole.error(`WIDGET MSG NODE | Failed to generate output - ${error}`);
+            return null;
+        }
+    }
+}
+
+export default widget_msg_send;
---
+++ trigger/widget_trigger/node.js
@@ -0,0 +1,79 @@
+import BaseNode from "../../core/BaseNode/node.js";
+
+const config = {
+    title: "Widget Trigger",
+    category: "trigger",
+    type: "widget_trigger",
+    icon: {},
+    desc: "Triggers the flow when a message is recieved from the support bot Widget",
+    inputs: [
+        {
+            desc: "Intro message shown by the bot",
+            name: "Intro",
+            type: "Text",
+        },
+    ],
+    outputs: [
+        {
+            desc: "The Flow to trigger",
+            name: "Flow",
+            type: "Flow",
+        },
+        {
+            desc: "Message recieved by the bot",
+            name: "Message",
+            type: "Text",
+        },
+    ],
+    fields: [
+        {
+            desc: "Intro message shown by the bot",
+            name: "Intro",
+            type: "Text",
+            value: `🎉 Welcome! I'm your AI assistant. This is a demo workflow. Ask me anything!`,
+        },
+    ],
+    difficulty: "easy",
+    tags: ["trigger", "support", "bot", 'widget'],
+}
+
+class widget_trigger extends BaseNode {
+    constructor() {
+        super(config);
+    }
+
+    async run(inputs, contents, webconsole, serverData) {
+        try {
+            webconsole.info("WIDGET NODE | Started execution");
+
+            const IntroFilter = inputs.find((e) => e.name === "Intro");
+            const Intro = IntroFilter?.value || contents.find((e) => e.name === "Intro")?.value || `🎉 Welcome! I'm your AI assistant. This is a demo workflow. Ask me anything!`;
+
+            const payload = serverData.chatbotPayload;
+            const msg = payload.Message || "";
+
+            const initReq = payload.init || false;
+
+            if (initReq) {
+                webconsole.success("WIDGET NODE | Recieved init request, responded successfully");
+                return {
+                    "Flow": false,
+                    "Message": "",
+                    "intro": Intro,
+                    "__terminate": true,
+                };
+            }
+
+            webconsole.success("WIDGET NODE | Message recieved, continuing flow");
+            return {
+                "Flow": true,
+                "Message": msg,
+            };
+        } catch (error) {
+            webconsole.error("WIDGET NODE | Some error occured");
+            return null;
+        }
+    }
+}
+
+export default widget_trigger;
