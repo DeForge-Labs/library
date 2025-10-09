985a2bf8f3b1dcd23afbdaa8a5fbdef9f23a402e
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2025-10-09 23:32:16 +0530

    Fix: Default output for num_var set to 0

--- input/num_var/node.js
+++ input/num_var/node.js
@@ -1,68 +1,77 @@
 import BaseNode from "../../core/BaseNode/node.js";
 
 const config = {
-    title: "Number Input",
-    category: "input",
-    type: "num_var",
-    icon: {},
-    desc: "Number variable",
-    credit: 0,
-    inputs: [],
-    outputs: [
-        {
-            name: "Number",
-            type: "Number",
-            desc: "",
-        }
-    ],
-    fields: [
-        {
-            name: "Number",
-            type: "Number",
-            desc: "",
-            value: 0,
-        },
-    ],
-    difficulty: "easy",
-    tags: ['int', 'float', 'number'],
-}
+  title: "Number Input",
+  category: "input",
+  type: "num_var",
+  icon: {},
+  desc: "Number variable",
+  credit: 0,
+  inputs: [],
+  outputs: [
+    {
+      name: "Number",
+      type: "Number",
+      desc: "",
+    },
+  ],
+  fields: [
+    {
+      name: "Number",
+      type: "Number",
+      desc: "",
+      value: 0,
+    },
+  ],
+  difficulty: "easy",
+  tags: ["int", "float", "number"],
+};
 
 class num_var extends BaseNode {
-    constructor() {
-        super(config);
+  constructor() {
+    super(config);
+  }
+
+  /**
+   * @override
+   * @inheritdoc
+   *
+   * @param {import("../../core/BaseNode/node.js").Inputs[]} inputs
+   * @param {import("../../core/BaseNode/node.js").Contents[]} contents
+   * @param {import("../../core/BaseNode/node.js").IWebConsole} webconsole
+   * @param {import("../../core/BaseNode/node.js").IServerData} serverData
+   */
+  async run(inputs, contents, webconsole, serverData) {
+    try {
+      if (contents.length === 0) {
+        webconsole.error("NUMBER INPUT | No input given");
+        return {
+          Number: 0,
+          Credits: this.getCredit(),
+        };
+      }
+
+      if ([NaN, undefined, null].includes(typeof contents[0].value)) {
+        webconsole.error("NUMBER NODE | Invalid input given");
+        return {
+          Number: 0,
+          Credits: this.getCredit(),
+        };
+      }
+
+      webconsole.info("NUMBER INPUT | emitting output value");
+      return {
+        Number: contents[0].value || 0,
+        Credits: this.getCredit(),
+      };
+    } catch (error) {
+      webconsole.error("NUMBER NODE | Some error occured: ", error);
+      return {
+        Number: 0,
+        Credits: this.getCredit(),
+      };
     }
-
-    /**
-     * @override
-     * @inheritdoc
-     * 
-     * @param {import("../../core/BaseNode/node.js").Inputs[]} inputs 
-     * @param {import("../../core/BaseNode/node.js").Contents[]} contents 
-     * @param {import("../../core/BaseNode/node.js").IWebConsole} webconsole 
-     * @param {import("../../core/BaseNode/node.js").IServerData} serverData
-     */
-    async run(inputs, contents, webconsole, serverData) {
-        try {
-            if (contents.length === 0) {
-                webconsole.error("NUMBER INPUT | No input given");
-                return null;
-            }
-
-            if ([NaN, undefined, null].includes(typeof(contents[0].value))) {
-                webconsole.error("NUMBER NODE | Invalid input given");
-                return null;
-            }
-
-            webconsole.info("NUMBER INPUT | emitting output value");
-            return {
-                "Number": contents[0].value,
-                "Credits": this.getCredit(),
-            };
-        } catch (error) {
-            webconsole.error("NUMBER NODE | Some error occured: ", error);
-            return null;
-        }
-    }
+  }
 }
 
 export default num_var;
